cb1d152a6f0b791f7125a2238ea0c7ee6727a4c1
Rakshith K N <150361593+rakshithkn1@users.noreply.github.com>
2024-02-05 12:54:11 +0530

    Add files via upload

---
+++ Background-color changer/in.js
@@ -0,0 +1,52 @@
+let c=document.getElementById("l1")
+let b=document.querySelector(".on")
+let d=document.querySelector("#color")
+c.addEventListener("click",()=>{
+b.style.backgroundColor="yellow"
+d.style.backgroundColor="yellow"
+})
+
+let c2=document.getElementById("l2")
+c2.addEventListener("click",()=>{
+    b.style.backgroundColor="red"
+    d.style.backgroundColor="red"
+})
+
+
+let c3=document.getElementById("l3")
+c3.addEventListener("click",()=>{
+    b.style.backgroundColor="green"
+    d.style.backgroundColor="green"
+})
+
+let c4=document.getElementById("l4")
+c4.addEventListener("click",()=>{
+    b.style.backgroundColor="blue"
+    d.style.backgroundColor="blue"
+})
+let c5=document.getElementById("l5")
+c5.addEventListener("click",()=>{
+    b.style.backgroundColor="violet"
+    d.style.backgroundColor="violet"
+})
+
+let c6=document.getElementById("l6")
+
+c6.addEventListener("click",()=>{
+    b.style.backgroundColor="aquamarine"
+    d.style.backgroundColor="aquamarine"
+})
+
+
+let c7=document.getElementById("l7")
+c7.addEventListener("click",()=>{
+    b.style.backgroundColor=" rgb(255, 115, 0)"
+    d.style.backgroundColor=" rgb(255, 115, 0)"
+})
+
+
+let c8=document.getElementById("l8")
+c8.addEventListener("click",()=>{
+    b.style.backgroundColor="rgb(0, 136, 255)"
+    d.style.backgroundColor="rgb(0, 136, 255)"
+})
